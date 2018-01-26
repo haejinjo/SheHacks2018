498107592f8336342606321c64031bb57dbabad8
Haejin Jo <professionalhaejin@gmail.com>
2018-01-26 10:49:50 -0800

    created xcodeproj

---
+++ SheHacks2018/SheHacks2018/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SheHacks2018
+//
+//  Created by Admin on 1/26/18.
+//  Copyright © 2018 Haejin Jo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
